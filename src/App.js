1f3d07d801ab5266a7787343d5f7431570f6a720
eunseong <klaqwe1@naver.com>
2024-03-23 23:41:35 +0900

    [feat] firebase 개발

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import './App.css';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { signInWithGoogle, signUpWithEmail, signInWithEmail } from './fbase';
+import Button
+ from './components/Button';
+const App = () => {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [error, setError] = useState("")
 
+  const handleGoogleSignIn = () => {
+    signInWithGoogle();
+  }
+  const handleEmailSignIn = () => {
+    signInWithEmail(email, password);
+  }
+  const handleEmailSignUp = () => {
+    // 이메일과 비밀번호를 사용하여 회원가입 시도
+    signUpWithEmail(email, password).catch((errorMessage) => {
+        setError(errorMessage);
+      });
+  }
 
-function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <>
+      <h2>Google login</h2>
+      <button onClick={handleGoogleSignIn}>Google로 회원가입</button>
+      <h2> Email login</h2>
+      <div>
+        <input
+          type="email"
+          placeholder = "이메일"
+          value = {email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) =>setPassword(e.target.value)}
+        />
+      </div>
+      <Button onClick={handleEmailSignUp} size={"md"} children={"Email로 회원가입"}></Button>
+      <Button onClick= {handleEmailSignIn} size={"md"} children={"Email로 로그인"} ></Button>
+      <Button size={"lg"} children={"완료"}></Button>
+    
+      {error && <p style={{color : 'red'}}>{error}</p>}
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/fbase.js
@@ -0,0 +1,77 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword,createUserWithEmailAndPassword } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGCUFHlGXGyKUe5Th_yQdxDCgvNjEEFBg",
+  authDomain: "yato-f7402.firebaseapp.com",
+  projectId: "yato-f7402",
+  storageBucket: "yato-f7402.appspot.com",
+  messagingSenderId: "804087797017",
+  appId: "1:804087797017:web:801e979dacda8e434ee188",
+  measurementId: "G-DB5E2P3TS5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); //변경
+
+export const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+
+
+}
+
+export const signUpWithEmail = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // 회원가입 성공 시 추가적인 작업 수행
+      const user = userCredential.user;
+      console.log("회원가입 성공:", user.uid);
+    })
+    .catch((error) => {
+      // 회원가입 실패 시 처리
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("회원가입 실패:", errorMessage);
+    });
+};
+
+export const signInWithEmail = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // 로그인 성공 시 추가적인 작업 수행
+      const user = userCredential.user;
+      console.log("로그인 성공:", user.uid);
+    })
+    .catch((error) => {
+      // 로그인 실패 시 처리
+      switch (error.code) {
+        case "auth/user-not-found" || "auth/wrong-password":
+          return "이메일 혹은 비밀번호가 일치하지 않습니다.";
+        case "auth/email-already-in-use":
+          return "이미 사용 중인 이메일입니다.";
+        case "auth/weak-password":
+          return "비밀번호는 6글자 이상이어야 합니다.";
+        case "auth/network-request-failed":
+          return "네트워크 연결에 실패 하였습니다.";
+        case "auth/invalid-email":
+          return "잘못된 이메일 형식입니다.";
+        case "auth/internal-error":
+          return "잘못된 요청입니다.";
+        default:
+          return "로그인에 실패 하였습니다.";
+      }
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("로그인 실패:", errorMessage);
+    });
+};
+
+export default {signInWithEmail, signUpWithEmail, signInWithGoogle}
